61c9e8d85d9ebd29cca2fc67d4196f35e92f30aa
Nurlybek Karshiga <nurlybekkarshiga@gmail.com>
2020-07-14 09:13:38 -0400

    Initial Commit

---
+++ CarsInCountries/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarsInCountries
+//
+//  Created by Nulrybek Karshyga on 7/14/20.
+//  Copyright © 2020 Nulrybek Karshyga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
